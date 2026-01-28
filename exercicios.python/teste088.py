09caeeb53dde8eaf56cfb4d7d3dcf4a7b9981da7
Giovanna Honda <arizonohonda@gmail.com>
2026-01-28 19:14:30 +0900

    teste 088

--- exercicios.python/teste088.py
+++ exercicios.python/teste088.py
@@ -1,15 +1,18 @@
-from random import randint
-lista = []
-quant = int(input("Quantos jogos vc quer queu sorteie? "))
-cont = 0
-print("-=" * 30)
-print("JOGO  MEGA SENA".center(40))
-print("-=" * 30)
-while True:
-    num = randint(1, 60)
-    if num not in lista:
-        lista.append(num)
-        cont += 1
-    if cont >= 6:
-        break
-print(lista)
+from random import sample
+from time import sleep
+jogos = []
+print("-=" * 20)
+print("JOGO DA MEGA SENA".center(40))
+print("-=" * 20)
+print()
+quant = int(input("Quantos jogos vc quer que eu sorteie? "))
+print()
+print("-=" * 5, f"SORTEANDO {quant} JOGOS", "-=" * 5)
+print()
+for n in range(quant):
+    jogos.append(sample(range(1, 61), 6))
+    jogos[n].sort()
+    print(f"jogo {n + 1}: {jogos[n]}") 
+    sleep(1)
+print()
+print("-=" * 5, "<BOA SORTE!>", "-=" * 5)
