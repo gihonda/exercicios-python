0df7f3dd090cf9db89dfd4780ac0fed783fa6b20
Giovanna Honda <arizonohonda@gmail.com>
2026-01-30 20:38:23 +0900

    update

--- exercicios.python/teste050.py
+++ exercicios.python/teste050.py
@@ -1,8 +1,7 @@
-soma = 0
-cont = 0
-for num in range(1,7):
-    num = int(input("Digite um numero: "))
+soma = cont = 0
+for n in range(1, 7):
+    num = int(input(f"Digite o {n} valor: "))
     if num % 2 == 0:
-        soma = soma + num
-        cont = cont + 1
-print(f"Vc digitou {cont} numeros pares e asoma foi {soma}")
+        soma += num
+        cont += 1
+print(f"Vove informou {cont} numeros pares e a soma foi {soma}.")
