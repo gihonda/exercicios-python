2c9e81d755995b2de7ca9aede30d899b16d0a3a3
Giovanna Honda <arizonohonda@gmail.com>
2026-02-13 20:50:40 +0900

    update

--- exercicios.python/teste092.py
+++ exercicios.python/teste092.py
@@ -1,17 +1,15 @@
 from datetime import datetime
-nome = str(input("Nome do funcionario: "))
-anos = int(input("Ano de nascimento: "))
-ctps = int(input("Carteira de trabalho (0 se nao tem): "))
-idade = datetime.now().year - anos
-funcionario = {"nome": nome, "idade": idade, "ctps": ctps}
+funcionario = str(input("Nome do funcionario: "))
+anonascimento = int(input("Ano de nascimento: "))
+ctps = int(input("Carteira de trabalho (0 se nao tiver): "))
+idade = datetime.now().year - anonascimento
+cadastro = {"nome": funcionario, "idade": idade, "ctps": ctps}
 if ctps != 0:
-    contratacao = int(input("Ano de contratacao: "))
+    ano_contratacao = int(input("Ano de contratacao: "))
     salario = float(input("Salario: R$"))
-    aposentadoria = (contratacao + 35) - anos
-    funcionario["contratacao"] = contratacao
-    funcionario["salario"] = salario
-    funcionario["aposentadoria"] = aposentadoria
+    cadastro["ano_contratacao"] = ano_contratacao
+    cadastro["salario"] = salario
+    cadastro["aposentadoria"] = ano_contratacao + 35 - anonascimento
 print("-=" * 30)
-for k, v in funcionario.items():
+for k, v in cadastro.items():
     print(f"  -{k} tem o valor {v}")
-    
